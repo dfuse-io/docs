cfe578928f3807143470dfb2767233e3132d3762
Olivier Beaulieu <olivierbeaulieu657@gmail.com>
2019-10-25 11:08:19 -0400

    Added GQL ref

--- content/reference/ethereum/graphql.md
+++ content/reference/ethereum/graphql.md
@@ -4,4 +4,4 @@ title: GraphQL Schema
 
 # GraphQL Schema
 
-{{ $tabName := index .Site.Data.tabs.labels $lang }}
+{{< partial graphql-reference >}}
--- graphql.js
+++ graphql.js
@@ -1,7 +1,33 @@
 const gql = require('graphql-tag');
 const fs = require('fs');
 
-const schema = fs.readFileSync('/Users/olivier/projects/dgraphql/eth/transactions.graphql', 'utf-8');
+const schemaSources = [
+  '../dgraphql/eth/subscription.graphql',
+  '../dgraphql/eth/query.graphql',
+  '../dgraphql/eth/transactions.graphql',
+  '../dgraphql/eth/block.graphql',
+]
+
+console.log(`Generating GraphQL schema data from source files:
+${schemaSources.map(path => ` * ${path}`).join('\n')}`);
+
+const schema = schemaSources.map(path => fs.readFileSync(path, 'utf-8')).join(' ')
+
+// Recursively parses the field name
+function getFieldType(field) {
+  const { type, kind, name } = field;
+
+  if (kind === 'NonNullType') {
+    return `${getFieldType(type)}!`
+  }
+  if (kind === 'ListType') {
+    return `[${getFieldType(type)}]`
+  }
+
+  return name.value;
+}
+
+// Loop over the schema definitions, extracting interesting data
 const jsonSchema = gql(schema).definitions.map(def => {
   const { name, description, fields } = def;
 
@@ -10,7 +36,20 @@ const jsonSchema = gql(schema).definitions.map(def => {
   return {
     name: name.value,
     description: description ? description.value : '',
+    fields: fields.map(field => {
+      const { name, type, description } = field;
+      
+      let typeName = getFieldType(type)
+
+      return {
+        name: name.value,
+        type: typeName,
+        description: description ? description.value : ''
+      }
+    })
   }
 }).filter(x => !!x);
+
+console.log('Done!')
 
 fs.writeFileSync('data/graphql.json', JSON.stringify(jsonSchema, null, 2));
