64ffc19d1ebd9265ee4835926a0c1fc8ceeb53d2
Alexandre Bourget <alex@bourget.cc>
2019-11-02 01:47:21 -0400

    Added support for linking of GraphQL, split in even smaller chunks. Digs deeper in the requiredness, in the lists format, etc..
    Support for inserting a `graphql-field` or list of items within another page,
    and specifying the link to it.
    
    Indent stuff so it looks slightly more hierarchical.

--- graphql.js
+++ graphql.js
@@ -19,21 +19,22 @@ function processSources(schemaSources, destination) {
       name: name.value,
       description: description ? description.value : '',
       fields: fields.map(field => {
-        const { name, type, description, arguments } = field;
-
-        let typeName = getFieldType(type)
+        const { name, type, description, arguments, defaultValue } = field;
+        console.log("MAMA", JSON.stringify(field))
+        let typeDef = getFieldType(type)
 
         return {
           name: name.value,
-          type: typeName,
+          type: typeDef,
           description: description ? description.value : '',
           arguments: arguments.map(arg => {
             const  { name, type, description, defaultValue } = arg;
 
-            let typeName = getFieldType(type)
+            let typeDef = getFieldType(type, defaultValue)
+
             return {
               name: name.value,
-              type: typeName,
+              type: typeDef,
               description: description ? description.value : '',
             }
           })
@@ -46,22 +47,66 @@ function processSources(schemaSources, destination) {
 }
 
 // Recursively parses the field name
-function getFieldType(field) {
+function getFieldType(field, defaultValue) {
   const { type, kind, name } = field;
-  console.log("BVOOOOOOOOOOOOOO", field);
+  console.log("BVOOOOOOOOOOOOOO", defaultValue);
 
-  switch (kind) {
-  case 'NonNullType':
-    return `${getFieldType(type)}!`
-  case 'ListType':
-    return `[${getFieldType(type)}]`
-  case 'NamedType':
-    return name.value
-  default:
-    throw("Wutz that kind?", kind)
+
+  if (field.kind === 'NonNullType') {
+    if (field.type.kind === 'ListType') {
+      if (field.type.type.kind === 'NonNullType') {
+        if (field.type.type.type.kind === 'NamedType') {
+          return {isList: true, reqList: true, reqType: true, name: field.type.type.type.name.value}
+        } else {
+          throw("unsupported path type:", field.type.type.type.kind)
+        }
+      } else if (field.type.type.kind === 'NamedType') {
+        return {isList: true, reqList: true, name: field.type.type.name.value}
+      } else {
+          throw("unsupported path type:", field.type.type.kind)
+      }
+    } else if (field.type.kind === 'NamedType') {
+      return {reqType: true, name: field.type.name.value}
+    } else {
+      throw("unsupported path type:", field.type.kind)
+    }
+  } else if (field.kind === 'ListType') {
+    if (field.type.kind === 'NonNullType') {
+      if (field.type.type.kind === 'NamedType') {
+        return {isList: true, reqType: true, name: field.type.type.name.value}
+      } else {
+        throw("unsupported path type:", field.type.type.kind)
+      }
+    } else if (field.type.kind === 'NamedType') {
+      return {isList: true, name: field.type.name.value}
+    } else {
+      throw("unsupported path type:", field.type.kind)
+    }
+  } else if (field.kind === 'NamedType') {
+    const out = {name: field.name.value}
+    if (defaultValue !== undefined) {
+      out.default = formatDefaultValue(defaultValue)
+    }
+    return out
+  } else {
+    throw("unsupported path type:", field.kind)
   }
 }
 
+function formatDefaultValue(def) {
+  switch (def.kind) {
+  case 'IntValue':
+    return def.value
+  case 'StringValue':
+    return JSON.stringify(def.value)
+  case 'BooleanValue':
+    return def.value
+  case 'EnumValue':
+    return def.value
+  default:
+    throw("unsupported format value:", def.kind)
+  }
+}
 
 processSources([
   '../dgraphql/eth/subscription.graphql',
